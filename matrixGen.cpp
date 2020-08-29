3a657e750ba19d2ac24d41a307458f6c135f5574
parkcitymedia <blakemichaelgaynor@gmail.com>
2020-08-29 14:21:50 -0700

    object-orient the world generation

--- matrixGen.cpp
+++ matrixGen.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // handles input & output
 #include <stdlib.h> // random number stuff
-#include <time.h> // used to seed random numbers
+#include <time.h>   // used to seed random numbers
 
 using namespace std; // makes shit work with less typing
 
@@ -8,31 +8,37 @@ int main() {
   // initialize random seed
   srand(time(NULL));
 
-  int worldSize, worldSizeCap;
-  worldSize = 2;     // initial value
-  worldSizeCap = 126; // default world size cap
+  class matrixWorld {
+  private:
+    int worldSize = 2;
+    const int worldSizeMax = 126;
+
+  public:
+    string generate() {
+      if (worldSize > worldSizeMax) { // check if world size is bigger than 16
+        cout << "world size can be no larger than " << to_string(worldSizeMax);
+      } else {
+        // generate world of size worldSize
+        cout << "generating world of size [" << to_string(worldSize)
+             << "] with [" << worldSize * worldSize * worldSize
+             << "] elements...\n";
+        int world[worldSize][worldSize][worldSize];
+        for (int xpos = 0; xpos < worldSize; xpos++) {
+          for (int ypos = 0; ypos < worldSize; ypos++) {
+            for (int zpos = 0; zpos < worldSize; zpos++) {
+              world[xpos][ypos][zpos] = rand() % 4;
+              cout << "world[" << to_string(xpos) << "," << to_string(ypos)
+                   << "," << to_string(zpos)
+                   << "]: " << to_string(world[xpos][ypos][zpos]) << "\n";
+            }
+          }
+        }
+      }
+      return 0;
+    }
+  };
 
   // specify world size
   cout << "world size: ";
   cin >> worldSize;
-
-  // generate world
-  if (worldSize > worldSizeCap) { // check if world size is bigger than 16
-    cout << "world size can be no larger than " << to_string(worldSizeCap);
-  } else {
-    // generate world of size worldSize
-    cout << "generating world of size [" << to_string(worldSize) << "] with ["
-         << worldSize * worldSize * worldSize << "] elements...\n";
-    int world[worldSize][worldSize][worldSize];
-    for (int xpos = 0; xpos < worldSize; xpos++) {
-      for (int ypos = 0; ypos < worldSize; ypos++) {
-        for (int zpos = 0; zpos < worldSize; zpos++) {
-          world[xpos][ypos][zpos] = rand() % 4;
-          cout << "world[" << to_string(xpos) << "," << to_string(ypos) << ","
-               << to_string(zpos) << "]: " << to_string(world[xpos][ypos][zpos])
-               << "\n";
-        }
-      }
-    }
-  }
 }
